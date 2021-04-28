c7f3bcb134e2d8501c8935988462ada404478363
Park hanjun <park.hanjun00@gmail.com>
2021-04-28 23:15:50 +0900

    setting 추가

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 // server.js
-const { createServer } = require("http");
-const { parse } = require("url");
-const next = require("next");
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
 
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -14,15 +14,9 @@ app.prepare().then(() => {
         const parsedUrl = parse(req.url, true);
         const { pathname, query } = parsedUrl;
 
-        if (pathname === "/a") {
-            app.render(req, res, "/a", query);
-        } else if (pathname === "/b") {
-            app.render(req, res, "/b", query);
-        } else {
-            handle(req, res, parsedUrl);
-        }
+        handle(req, res, parsedUrl);
     }).listen(3000, (err) => {
         if (err) throw err;
-        console.log("> Ready on http://localhost:3000");
+        console.log('> Ready on http://localhost:3000');
     });
 });
