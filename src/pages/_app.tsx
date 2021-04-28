dbfcaf03591d29355246cbbe55455de3176fa735
elliot-p <elliot.p@kakaocorp.com>
2021-04-28 17:45:37 +0900

    린트 설정 테스트중

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import App from "next/app";
+import type { AppProps, AppContext } from "next/app";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />;
+}
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+    const appProps = await App.getInitialProps(appContext);
+
+    return { ...appProps };
+};
+export default MyApp;
